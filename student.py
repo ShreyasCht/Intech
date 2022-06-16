54dd51e758b7654b139e87a85ebff90648b830ea
ShreyasCht <shreyas.chatterjee@ais.amity.edu>
2022-06-16 22:34:07 +0530

    1 change

--- student.py
+++ student.py
@@ -1,3 +1,6 @@
+import os
+
+
 def student_login():
     import sys
     import mysql.connector as ms
@@ -11,7 +14,7 @@ def student_login():
     connection = ms.connect(host="localhost", user="root", passwd="Shreyas25%")
     cur = connection.cursor()
 
-    def open_d():
+    def open():
         """Function To Open Connection To Database"""
         try:
             cur.execute("use {}".format(database_name))  # Name of my database is 'database_name'
@@ -42,25 +45,8 @@ def student_login():
             frame2.destroy()
         except:
             pass
-        root.geometry("950x400")
-        frame2 = tk.LabelFrame(root, bg="#414F4B", text="View Homework", padx=10, pady=10, font=("Verdana", 10, "bold"))
-        frame2.grid(row=2, column=1, padx=10, pady=10)
-        tk.Button(frame2, text="View All Homework", command=vah, font=("David", 10, "bold")) \
-            .grid(row=0, column=0, padx=5, pady=5)
-        tk.Button(frame2, text="View Pending Homework", command=vph, font=("David", 10, "bold")) \
-            .grid(row=0, column=1, padx=5, pady=5)
-        tk.Button(frame2, text="View Submitted Homework", command=vsh, font=("David", 10, "bold")) \
-            .grid(row=0, column=2, padx=5, pady=5)
-
-    def vah():
-        """Function To Show Submenu Of View All Homework"""
-        global frame2
-        try:
-            frame2.destroy()
-        except:
-            pass
-        root.geometry("950x400")
-        frame2 = tk.LabelFrame(root, bg="#414F4B", text="View All Homework", padx=10, pady=10,
+        root.geometry("700x400")
+        frame2 = tk.LabelFrame(root, bg="#414F4B", text="View Homework", padx=10, pady=10,
                                font=("Verdana", 10, "bold"))
         frame2.grid(row=2, column=1, padx=10, pady=10)
         tk.Button(frame2, text="Physics", command=p_a_hw, width=25, font=("David", 10, "bold")) \
@@ -71,67 +57,53 @@ def student_login():
             .grid(row=0, column=2, padx=5, pady=5)
 
     def p_a_hw():
-        pass
+        view_asgnhw('P')
 
     def c_a_hw():
-        pass
+        view_asgnhw('C')
 
     def m_a_hw():
-        pass
+        view_asgnhw('M')
 
-    def vph():
-        """Function To Show Submenu Of View Pending Homework"""
-        global frame2
-        try:
-            frame2.destroy()
-        except:
-            pass
-        root.geometry("950x400")
-        frame2 = tk.LabelFrame(root, bg="#414F4B", text="View Pending Homework", padx=10, pady=10,
-                               font=("Verdana", 10, "bold"))
-        frame2.grid(row=2, column=1, padx=10, pady=10)
-        tk.Button(frame2, text="Physics", command=p_p_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=0, padx=5, pady=5)
-        tk.Button(frame2, text="Chemistry", command=c_p_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=1, padx=5, pady=5)
-        tk.Button(frame2, text="Maths", command=m_p_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=2, padx=5, pady=5)
+    def view_asgnhw(id):
+        open()
+        Display = tk.Toplevel()
+        Display.title("All Records")
+        frame_d = tk.LabelFrame(Display)
+        frame_d.grid(row=0, column=0, padx=10, pady=10)
+        cur.execute("select * from assignment where hw_id like '{}'".format(id + '%'))
+        rows = cur.fetchall()
+        if len(rows) == 0:
+            Display.destroy()
+            response = messagebox.showerror("WARNING", "No Homework Added yet")
+            return ()
 
-    def p_p_hw():
-        pass
+        # Scroll Bar
+        def myfunction(event):
+            canvas.configure(scrollregion=canvas.bbox("all"), width=600, height=250)
 
-    def c_p_hw():
-        pass
+        canvas = tk.Canvas(frame_d)
+        frame_d_l = tk.LabelFrame(canvas, bd=0, padx=10, pady=10)
+        myscrollbar = tk.Scrollbar(frame_d, orient='vertical', command=canvas.yview)
+        myscrollbar2 = tk.Scrollbar(frame_d, orient='horizontal', command=canvas.xview)
+        canvas.configure(xscrollcommand=myscrollbar2.set)
+        canvas.configure(yscrollcommand=myscrollbar.set)
+        myscrollbar.pack(side="right", fill="y")
+        myscrollbar2.pack(side="bottom", fill="x")
+        canvas.pack(side="left")
+        canvas.create_window((0, 0), window=frame_d_l, anchor='nw')
+        frame_d_l.bind("<Configure>", myfunction)
 
-    def m_p_hw():
-        pass
-
-    def vsh():
-        """Function To Show Submenu Of View Submitted Homework"""
-        global frame2
-        try:
-            frame2.destroy()
-        except:
-            pass
-        root.geometry("950x400")
-        frame2 = tk.LabelFrame(root, bg="#414F4B", text="View Submitted Homework", padx=10, pady=10,
-                               font=("Verdana", 10, "bold"))
-        frame2.grid(row=2, column=1, padx=10, pady=10)
-        tk.Button(frame2, text="Physics", command=p_s_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=0, padx=5, pady=5)
-        tk.Button(frame2, text="Chemistry", command=c_s_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=1, padx=5, pady=5)
-        tk.Button(frame2, text="Maths", command=m_s_hw, width=25, font=("David", 10, "bold")) \
-            .grid(row=0, column=2, padx=5, pady=5)
-
-    def p_s_hw():
-        pass
-
-    def c_s_hw():
-        pass
-
-    def m_s_hw():
-        pass
+        col = ["Homework ID", "Homework", "Classes Assigned"]
+        for i in range(len(col)):
+            tk.Label(frame_d_l, text=col[i], font=("Ariel Bold", 12, "bold", "underline")) \
+                .grid(row=0, column=i, pady=10, padx=5)
+        j = 1
+        for r in rows:
+            for i in range(len(col)):
+                tk.Label(frame_d_l, text=r[i], font=("Times New Roman", 12, "italic")) \
+                    .grid(row=j, column=i, pady=10, padx=5)
+            j += 1
 
     def submit_homework():
         global frame2
@@ -139,22 +111,9 @@ def student_login():
             frame2.destroy()
         except:
             pass
+        os.system("cmd /c notepad")
 
     def view_submitted_homework():
-        global frame2
-        try:
-            frame2.destroy()
-        except:
-            pass
-
-    def report_card():
-        global frame2
-        try:
-            frame2.destroy()
-        except:
-            pass
-
-    def student_list():
         global frame2
         try:
             frame2.destroy()
@@ -167,6 +126,45 @@ def student_login():
             frame2.destroy()
         except:
             pass
+        open()
+        Display = tk.Toplevel()
+        Display.title("All Records")
+        frame_d = tk.LabelFrame(Display)
+        frame_d.grid(row=0, column=0, padx=10, pady=10)
+        cur.execute("select * from staff")
+        rows = cur.fetchall()
+        if len(rows) == 0:
+            Display.destroy()
+            response = messagebox.showerror("WARNING", "No Records Of Teacher Added Yet")
+            return ()
+
+        # Scroll Bar
+        def myfunction(event):
+            canvas.configure(scrollregion=canvas.bbox("all"), width=972, height=245)
+
+        canvas = tk.Canvas(frame_d)
+        frame_d_l = tk.LabelFrame(canvas, bd=0, padx=10, pady=10)
+        myscrollbar = tk.Scrollbar(frame_d, orient='vertical', command=canvas.yview)
+        myscrollbar2 = tk.Scrollbar(frame_d, orient='horizontal', command=canvas.xview)
+        canvas.configure(xscrollcommand=myscrollbar2.set)
+        canvas.configure(yscrollcommand=myscrollbar.set)
+        myscrollbar.pack(side="right", fill="y")
+        myscrollbar2.pack(side="bottom", fill="x")
+        canvas.pack(side="left")
+        canvas.create_window((0, 0), window=frame_d_l, anchor='nw')
+        frame_d_l.bind("<Configure>", myfunction)
+
+        col = ["Staff ID", "Name", "Phone", "Class-Teacher", " Subject", "Address", "Email-ID",
+               "Classes", "Date Of Birth"]
+        for i in range(len(col)):
+            tk.Label(frame_d_l, text=col[i], font=("Ariel Bold", 12, "bold", "underline")) \
+                .grid(row=0, column=i, pady=10, padx=5)
+        j = 1
+        for r in rows:
+            for i in range(len(col)):
+                tk.Label(frame_d_l, text=r[i], font=("Times New Roman", 12, "italic")) \
+                    .grid(row=j, column=i, pady=10, padx=5)
+            j += 1
 
     def close():
         """"Function To Exit The Application"""
@@ -207,26 +205,20 @@ def student_login():
     tk.Button(frame, text="Personal Records", command=personal_records, width=25,
               font=("Cascadia Code SemiBold", 10, "bold")).grid(row=0, column=0, padx=8, pady=15)
 
-    tk.Button(frame, text="View Homework", command=view_homework, width=25,
+    tk.Button(frame, text="View Homeworks", command=view_homework, width=25,
               font=("Cascadia Code SemiBold", 10, "bold")).grid(row=0, column=1, padx=8, pady=15)
 
     tk.Button(frame, text="Submit Homework", command=submit_homework, width=25,
               font=("Cascadia Code SemiBold", 10, "bold")).grid(row=0, column=2, padx=8, pady=15)
 
     tk.Button(frame, text="View Homework Answers", command=view_submitted_homework, width=25,
-              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=0, column=3, padx=8, pady=15)
-
-    tk.Button(frame, text="Report Card", command=report_card, width=25,
-              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=0, padx=8, pady=15)
-
-    tk.Button(frame, text="Student List", command=student_list, width=25,
               font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=1, padx=8, pady=15)
 
     tk.Button(frame, text="Teachers List", command=teachers_list, width=25,
-              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=2, padx=8, pady=15)
+              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=0, padx=8, pady=15)
 
     tk.Button(frame, text="Exit The Program", command=close, width=25,
-              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=3, padx=8, pady=15)
+              font=("Cascadia Code SemiBold", 10, "bold")).grid(row=1, column=2, padx=8, pady=15)
 
     root.mainloop()
     connection.close()
